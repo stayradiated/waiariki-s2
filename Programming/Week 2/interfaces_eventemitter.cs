45c40b6b6b161d49151a9aede6098513225c0325
George Czabania <george@czabania.com>
2014-07-25 14:13:29 +1200

    Add comments

--- Programming/Week 2/interfaces_eventemitter.cs
+++ Programming/Week 2/interfaces_eventemitter.cs
@@ -4,17 +4,32 @@ using System.Text;
 
 namespace Interfaces {
 
+  /*
+   * EventListner Delegate
+   * Callbacks must use this signature
+   */
+
   public delegate void EventListener(object data);
 
+  /*
+   * IEventEmitter Interface
+   * - Emit: trigger an event
+   * - On: listen to an event
+   * - Off: stop listening to an event
+   */
+
   public interface IEventEmitter {
-
     void Emit(string name, object data);
-
     void On(string name, EventListener callback);
-
     void Off(string name, EventListener callback);
-
   }
+
+
+  /*
+   * EventEmitter Implementation
+   * Uses a dictonary to store event names
+   * Callbacks are stored in lists.
+   */
 
   public class Signals : IEventEmitter {
 
@@ -45,20 +60,24 @@ namespace Interfaces {
         this.Events[name].Remove(callback);
       }
     }
-
   }
 
   public class Program {
     public static void Main(string[] args) {
+
       Signals signals = new Signals();
 
+      // subscribe to an event
       signals.On("event", Program.PrintText);
 
+      // trigger an event
       signals.Emit("event", "This is sending some data to the thing");
 
+      // unsubscribe to the event
       signals.Off("event", Program.PrintText);
 
-      signals.Emit("event", "This is some more data");
+      // trigger the event again
+      signals.Emit("event", "You shouldn't see this");
 
       Console.Read();
     }
